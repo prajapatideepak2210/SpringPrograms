9fae7431415ea249959a9ea409683d6995016909
sunil965clear <sunilkumar192114@gmail.com>
2017-11-18 18:53:25 +0530

    commit

--- LoginRegistration/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body bgcolor="#FFEFD5">
-<h2 align="center">WelCome Login & Registration</h2>
-	<a href="Login">Login</a> | <a href="Registration">Registration</a>
-</body>
-</html>
--- LoginregistrationProject/src/main/java/com/bridgelabz/Dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.Dao;
-
-import com.bridgelabz.model.Login;
-import com.bridgelabz.model.User;
-
-public interface UserDao {
-  int register(User user);
-  User validateUser(Login login);
-}
